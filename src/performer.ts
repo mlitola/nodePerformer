d6bf41b9c71fc611e54ccb255493ec0f80c28a0d
Mikko Litola <mikko.litola@gmail.com>
2024-06-18 13:43:25 +0300

    Added basic html serving and dotenv usage

--- src/performer.ts
+++ src/performer.ts
@@ -1,14 +1,14 @@
 import express, { Express, Request, Response } from "express";
+import path from "path";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const performer : Express = express();
-const port = process.env.Port || 8080;
+const port = process.env.PORT || 8080;
 
 performer.get('/', (req : Request, res: Response) => {
-  res.send('Node Performer');
-  // TODO: list default endpoints here
+  res.sendFile(path.resolve(process.env.HTMLPATH));
 });
 
 performer.listen(port, () => {
