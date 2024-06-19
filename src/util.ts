37b71831cb19007b58badd76d4f6eec1a7c83400
Mikko Litola <mikko.litola@gmail.com>
2024-06-19 21:23:20 +0300

    Added mocha and unit tests for util

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,10 @@
 export const GenerateRandomNumbers = (dataSize: number, maxVal: number): number[] => {
     const randIntArr : number[] = [];
 
+    if (dataSize < 1) {
+        return [];
+    }
+    
     for (let i = 0; i < dataSize; i++) {
         const val = Math.floor(Math.random() * maxVal);
         randIntArr.push(val);
---
+++ test/util.spec.ts
@@ -0,0 +1,32 @@
+import { assert, expect } from "chai";
+import { GenerateRandomNumbers } from "../src/util";
+
+describe("Util Tests", () => {
+   describe("GenerateRandomNumbers Tests", () => {
+      it("should generate random number array of specific length", () => {
+         const numbers = GenerateRandomNumbers(10, 10);
+         assert.equal(numbers.length, 10);
+      });
+
+      it("should generate random numbers that are numbers", () => {
+         const numbers = GenerateRandomNumbers(10, 10);
+         for (const item of numbers) {
+            assert.isNotNaN(item);
+         } 
+      });
+
+      it("should generate random numbers that are in specific range", () => {
+         const numbers = GenerateRandomNumbers(10, 10);
+         for (const item of numbers) {
+            expect(item).not.to.be.below(0);
+            expect(item).to.be.below(11);
+         }
+
+      });
+
+      it("generate random numbers return empty array for smaller than one data size", () => {
+         const numbers = GenerateRandomNumbers(0, 10);
+         assert.equal(numbers.length, 0); 
+      });
+   });
+});
