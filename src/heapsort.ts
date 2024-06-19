86892ea3810124697f48ee387a2d3dccaf76dd9e
Mikko Litola <mikko.litola@gmail.com>
2024-06-19 16:38:29 +0300

    Added heap sort algorithm functionality

---
+++ src/heapsort.ts
@@ -0,0 +1,55 @@
+import { MaxNumberValue, SecondInMicroseconds } from "./constants.js";
+import {  GenerateRandomNumbers } from "./util.js"
+
+export const benchmarkHeapsort = (dataSize: number): number => {
+    if (dataSize <= 1) {
+        return 0;
+    }
+    
+    const randomNumberArray : number[] = GenerateRandomNumbers(dataSize, MaxNumberValue);
+
+    const startTime = process.hrtime();
+
+    heapsort(randomNumberArray, dataSize);
+
+    const diff = process.hrtime(startTime);
+
+    // return seconds of the time it took to execute the algorithm
+    return (diff[0] * 1000000 + diff[1] / 1000) / SecondInMicroseconds;
+}
+const heapsort = (dataArray: number[], dataSize: number) => {
+    for (let i = Math.floor(dataSize / 2) - 1; i >= 0; i--) {
+        createHeapFromArray(dataArray, dataSize, i);
+    }
+  
+    for (let i = dataSize - 1; i > 0; i--) {
+        const swapValue = dataSize[0];
+        dataArray[0] = dataArray[i];
+        dataArray[i] = swapValue;
+
+        createHeapFromArray(dataArray, i, 0);
+    }
+}
+
+const createHeapFromArray = (dataArray: number[], dataSize: number, index: number) => {
+    const left = 2 * index + 1;
+    const right = 2 * index + 2;
+
+    let largestValue = index;
+
+    if  (left < dataSize && dataArray[left] > dataArray[largestValue]) {
+        largestValue = left;
+    }
+
+    if  (right < dataSize && dataArray[right] > dataArray[largestValue]) {
+        largestValue = left;
+    }
+
+    if (largestValue != index) {
+        const swapValue = dataArray[index];
+        dataArray[index] = dataArray[largestValue];
+        dataArray[largestValue] = swapValue;
+
+        createHeapFromArray(dataArray, dataSize, largestValue);
+    }
+}
--- src/performer.ts
+++ src/performer.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import { benchmarkBubblesort } from "./bubblesort.js";
+import { benchmarkHeapsort } from "./heapsort.js";
 import { benchmarkMergesort } from "./mergesort.js"
 import { benchmarkQuicksort } from "./quicksort.js";
 import { Million, HundredThousand } from "./constants.js";
@@ -11,8 +12,6 @@ dotenv.config();
 const performer : Express = express();
 const port = process.env.PORT || 8080;
 
-const dataSize = Million;
-
 performer.get('/', (req : Request, res: Response) => {
   res.sendFile(path.resolve(process.env.HTMLPATH));
 });
@@ -20,24 +19,30 @@ performer.get('/', (req : Request, res: Response) => {
 performer.get('/bubblesort', (req : Request, res: Response) => {
   // Using smaller data size for bubble sort, as it is the slowest 
   const secs = benchmarkBubblesort(HundredThousand);
-  sendResponse(res, secs, "Bubblesort");
+  sendResponse(res, secs, "Bubblesort", HundredThousand);
 });
 
 performer.get('/heapsort', (req : Request, res: Response) => {
-  res.send("TBD...");
+  const secs = benchmarkHeapsort(Million);
+  sendResponse(res, secs, "Heapsort", Million);
 });
 
 performer.get('/mergesort', (req : Request, res: Response) => {
-  const secs = benchmarkMergesort(dataSize);
-  sendResponse(res, secs, "Mergesort");
+  const secs = benchmarkMergesort(Million);
+  sendResponse(res, secs, "Mergesort", Million);
 });
 
 performer.get('/quicksort', (req : Request, res: Response) => {
-  const secs = benchmarkQuicksort(dataSize);
-  sendResponse(res, secs, "Quicksort");
+  const secs = benchmarkQuicksort(Million);
+  sendResponse(res, secs, "Quicksort", Million);
 });
 
-const sendResponse = (res: Response, seconds: number, algorithmName: string) => {
+const sendResponse = (
+  res: Response,
+  seconds: number,
+  algorithmName: string,
+  dataSize: number
+) => {
   const msg = `${algorithmName} for data size: ${dataSize} took ${seconds.toFixed(8)} seconds.`;
   
   console.log(msg);
